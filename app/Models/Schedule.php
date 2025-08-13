dd696635ff310142a8c1fb391283d78b3dc94211
Rodrigo Gonzalez <drodro337@gmail.com>
2025-08-13 13:04:35 -0600

    Changes to be committed: 	modified:   app/Models/Schedule.php

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -80,3 +80,85 @@ En serio por donde lo veas es una leyenda muy rota 😕que te da mucha, muuuucha
 Deberían ser ajustadas a mas tiempo o que mientras lo uses no te puedas mover, 😉que la visión se te nuble o que si te hacen daño este se cancele, por donde lo veas es una leyenda muy injusta🙂🙂, añadiendo el % exagerado de selección que tendra la primer semana de lanzamiento, en serio me voy a encontrar hasta 1 en cada pelotón wtf?!😁😁😁
 No no es bait, 😙😙por algo en Fortnite removieron el termo escudo  .... Ehhh .. emmm....
 
+Copypastas de R6 que me encuentro por ahí
+ 
+jajaja, me da risa la gente pobre, cuando me hablan siempre trato de no vomitar por respeto a este grupo de pobres aveces me pongo a pensar en como le harán para poder vivir siendo pobres, pero bueno que les puedo decir tengo una buena xbox series x que la mayoría de ustedes los pobres solo verán por foto, tengo una good vida y tengo buen rango por eso aveces siento que debería irme a vivir a París ahí no hay pobres jajajaja
+
+Nerfeen a Caveira. Siempre tiene que haber una en el equipo contrario para destrozarte los planes y sorprenderte por atrás (como siempre). No saben hacer más nada. Es un dolor de cabeza. Por ejemplo, hagan que sus pasos silenciosos duren menos tiempo. Ya dura demasiado. Pueden cruzar medio mapa con su habilidad.
+
+Jajaja , no te has puesto a pensar en el mañana amigo no estarás toda la vida en ese juego.
+¿Crees que jugando todos los días lograrás mejorar?
+Talvez lo logres , pero mientras vas perdiendo tu tiempo lográndolo yo ya estaré terminando la universidad siendo un profesional; sin embargo entiendo que la superación no es relevante en tu vida te irás dando cuenta de eso conforme a tu madurez, pero con ese post tuyo me demuestras que eres un simple niño sin propósitos de superación.
+
+Deberian de poner un buff a finka que con un buffo te recuperará toda la vida y además deberían subir las cargas por qué es injusto para los atacantes por qué los defensores pueden controlar la vida que tienen lo que no pasa con los atacantes
+
+Hahahaha como si te ardió.
+Cabron, no es por presumir pero uso tenis de hasta 15,000. Uso 3 xbox para ver Netflix y el cuarto que es el personal y lo ocupa para netflix hahahaha, ocupo doble teléfono
+Uno para perder el tiempo con gente como tú y otro para mi familia... Solo que no tengo que andar ahí presumiendo hahaha.
+Y si te creo, pasaste de comprar componentes de segunda mano a calidad buena...
+Pero bueno, la diferencia entre tu y yo es que a mi no me lo compra mi papito...
+Cuando lavan mi ropa la identificó porque mis calcetas son lv y armani y cuando me preguntan que perfume use me puedo dar el lujo de decir gucci o marcas como esas..
+Puedo presumir todo eso pero con saber que solo son cosas materiales me da igual...
+Te puedo decir que los tenis que traigo ahorita cuestan 17,580 pero para que?
+
+Que feo que apenas lleve 4 meses jugando y le gane a los diamantes y uno que otro champion 
+En fin….
+Rainbow six siegezzzzzz
+
+Era mi novia y no era bait, pero que necesidad había de hacerla sentir así?
+
+Con lo mamon que eres seguro has de estudiar una pendejada como ING en Sistemas computacionales o Diseño Gráfico un título universitario no te resuelve la vida
+
+HAHA si es cierto este juego da tanta pena que yo ya le gane a spoit a ozzie y a shawk pero bueno tu solo le ganaste a unos campeonezzz y yo ya ando en ligas mayores papa te compro manos o que?
+
+Me enferma que todos usen esa Smg y se crean pros. No tiene chiste
+
+Tengo un año subiendo videos a este grupo no se crean tan importantes eso lo lei en cualquier video, que me aburra tener que explicar lo mismo siempre es diferente a que me arda, por dios tengo 30 años ya solo me divierto créeme que tengo mas cosas en que pensar .
+
+Estoy harto de que me toquen puros bots en rankeds , siendo un jugador con un gran KD y siendo bronce II me siguen tocando con puro manco acarreado , estoy harto del emparejamiento del juego siempre me toca jugar contra platas y mis compañeros de equipo no ayudan en nada , HOY MURIO ARCOIRIS SEIS
+
+Me da risa como todos defienden la serie solo porque es halo yo soy un fan de la franquicia  pero no por eso me deben gustar todos sus proyectos y lamentablemente esta serie se cagó en todo lo que significa el jefe maestro, Cortana y sus correspondientes facciones.
+
+Mejor sube tu pésimo rango y luego presumes a tus operadores
+
+Jajajajaja. Que triste que tú meta en la vida sea llegar lejos en un jueguito que en cualquier momento cierran o simplemente vas a tener que dejar de ser un vago y ponerte a trabajar. ¿Qué necesidad hay de trabajar hasta cuando tocas fondo?. Abre los ojos, no seas tonto, mientras tú presumes tu logro de tu jueguito yo estoy apunto de terminar mi tercera licenciatura, pero dudo mucho que un descerebrado como tú pueda entender que eso es más importante y valioso que tú pequeñes, yo supe invertir bien mi tiempo. Espero que abras los ojos, no puedes seguir viviendo engañado jajajajaja.
+
+Hahaha, you haven't thought about tomorrow, friend, you won't be in that game all your life.
+Do you think that by playing every day you will be able to improve?
+Maybe you will achieve it, but while you are wasting your time achieving it, I will already be finishing university as a professional; However, I understand that overcoming is not relevant in your life, you will realize that according to your maturity, but with that comment of yours you show me that you are a simple child with no goals of overcoming.
+
+哈哈哈，你没想到明天，朋友，你不会一辈子都在那个游戏里。
+你认为每天打球就能进步吗？
+也许你会实现它，但是当你在浪费时间实现它时，我已经完成了大学专业； 但是，我明白克服与你的生活无关，你会意识到根据你的成熟度，但你的评论告诉我你是一个没有克服目标的简单孩子。
+
+Hahaha, você não pensou no amanhã, amigo, você não vai estar nesse jogo a vida toda.
+ Você acha que jogando todos os dias você será capaz de melhorar?
+ Talvez você consiga, mas enquanto você está perdendo seu tempo conseguindo, eu já estarei terminando a universidade como profissional;  Porém, entendo que a superação não é relevante na sua vida, você vai perceber isso conforme sua maturidade, mas com esse seu comentário você me mostra que é uma criança simples sem objetivos de superação.
+
+Deberian quitar favela de rankeds, como es posible que este mapa siga en Rainbow, todo el mundo lo odia, si no lo quitan para la proxima semana me retiro y denuncio el juego por matar a la comunidad
+
+pubertos mecos creando cuentas para ser aceptados por desconocidos que les vale Vg sus inseguridades, salgan del cuarto niñatos.
+
+jajaja, me da risa que este fandom tiene a pura gente pobre y naca, cuando me hablan siempre trato de no vomitar por respeto a este grupo de pobres aveces me pongo a pensar en como le harán para poder vivir siendo pobres, pero bueno que les puedo decir tengo una buena xbox series x que la mayoría de ustedes los pobres solo verán por foto, tengo una good vida y tengo buen rango por eso aveces siento que debería irme a vivir a París ahí no hay pobres jajajaja
+
+HASTA YA ME QUEMARON NMMS, MEJOR TRABAJEN JODIDOS
+
+oro mugroso?.. yo no uso mnk te ganaría facilmente mi primate pero a diferencia de ti  estudio 4 hr diarias tengo novia  en los 2 años que  llevas jugando he logrado muchas cosas y tu no pasas de platino  si me das pena, llevo jugando 6 meses y me la p3lste
+
+Es que todo es psicológico pa, ¿No te has puesto a pensar lo interesante que es ser Main Fuze? Jugar con la mente del rival, hacerle escuchar el ruido de la perdición y el sufrimiento, escuchar como corren y temen por sus vidas sin retorno alguno, el man que usa al Fuze esta un paso adelante por que mientras tu decides acabar con el enemigo con balas el decide acabar con sus Demonios internos sembrando el pánico y el terror. Saludos a mi abuelo Eulogio
+
+Propongo crear otro grupo. Como antes, Donde se publiquen jugadas, videos y bromas pero no copy paste y gente con nombres de personajes. ABUURRENN
+
+A todos esos HDSPM que copian y pegan,les van a empezar a apodar "el primaverin", les voy a dejar el culo bien floreado malditos
+
+OJOOO con este orangután aparte de  que juega con el qlo se cree pro player y no pasa de platino 3 desde hace 6 temporadas, te mata y se te pone a bailar como teibolera.🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊
+
+Debería renunciar a tener novia? Juego un juego llamado Rainbow six desde hace 5 años, No logro salir de virgen, soy un chico que a los 30 años todavía no ha tenido relaciones sexuales, ni novia, y mucho menos he besado a una chica, estoy pensando en rendirme y aceptar que nunca tendré novia, a los 30 ya todos saben besar, y han tenido relaciones, no creo que pueda conseguir una novia porque ninguna mujer querrá estar con un chico que ni siquiera sabe besar, creo que ya estoy muy viejo  para tener esa primera experiencia, por lo que creo que lo mejor es renunciar y resignarme a que nunca tendré una novia.
+
+Creo que hablo en nombre de todos cuando digo que un buen buff para Nomad sería que cuando su habilidad te empuje, te saque del mapa, matándote instantaneamente.
+
+Una de las cosas que me parece la cosa mas injusta es NEWCASTLE. 🛡️🛡️
+En serio por donde lo veas es una leyenda muy rota 😕que te da mucha, muuuucha ventaja, te permite reanimar a un compañero demasiado rápido y ademas desplazarlo a un sitio seguro en el proceso 😒😒
+Deberían ser ajustadas a mas tiempo o que mientras lo uses no te puedas mover, 😉que la visión se te nuble o que si te hacen daño este se cancele, por donde lo veas es una leyenda muy injusta🙂🙂, añadiendo el % exagerado de selección que tendra la primer semana de lanzamiento, en serio me voy a encontrar hasta 1 en cada pelotón wtf?!😁😁😁
+No no es bait, 😙😙por algo en Fortnite removieron el termo escudo  .... Ehhh .. emmm....
+
